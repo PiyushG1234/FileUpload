2f1bee2adc070b2497ff08ed2fb6582ae59d9fcf
user.name like: "Piyush Gupta <user.email like: "ypiyushgupta@gmail.com>
2024-08-02 01:05:23 +0530

    structure-complete

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+require ("dotenv").config();
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGOBD_URL)
+    .then(console.log("DB Connection successful"))
+    .catch((error) =>{
+        console.log("DB connection issues");
+        console.log(error);
+        process.exit(1);
+    })
+}; 
